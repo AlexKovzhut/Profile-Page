340d4530a5cb41edc2a0cfdacd79bda4f372727a
Alexander Kovzhut <akovzhut@icloud.com>
2021-05-30 23:46:29 +0300

    Initial commit

---
+++ Profile Page/ViewControllers/ProfileController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileController.swift
+//  Profile Page
+//
+//  Created by Alexander Kovzhut on 30.05.2021.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
