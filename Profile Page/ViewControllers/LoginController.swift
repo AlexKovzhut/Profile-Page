3b6649d4d2ffe5fc95922c4a60037a4814501012
Alexander Kovzhut <akovzhut@icloud.com>
2021-06-01 23:34:44 +0300

    Initial commit

--- Profile Page/ViewControllers/LoginController.swift
+++ Profile Page/ViewControllers/LoginController.swift
@@ -8,38 +8,10 @@
 import UIKit
 
 class LoginController: UIViewController {
-    
-    @IBOutlet weak var loginTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var loginButton: UIButton!
-    
-    @IBOutlet weak var forgotLoginButton: UIButton!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let destinationVC = segue.destination as? WelcomePageController else { return }
-        destinationVC.login = loginTextField.text
-        }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    @IBAction func loginPressedButton(_ sender: UIButton) {
-        if loginTextField.text == "Admin" && passwordTextField.text == "Qwerty123" {
-        performSegue(withIdentifier: "loginSegue", sender: nil)
-        }
-    }
-    
-    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
-        guard segue.identifier == "unwindSegue" else { return }
-    }
-
 }
--- Profile Page/ViewControllers/WelcomePageController.swift
+++ Profile Page/ViewControllers/WelcomePageController.swift
@@ -8,21 +8,10 @@
 import UIKit
 
 class WelcomePageController: UIViewController {
-    @IBOutlet weak var greetingsLabel: UILabel!
-    
-    @IBOutlet weak var returnButton: UIButton!
-    
-    var login: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let login = self.login else { return }
-        greetingsLabel.text = "Welcome, \(login)"
 
-    }
-    
-    @IBAction func returnPressedButton(_ sender: UIButton) {
-        performSegue(withIdentifier: "unwindSegue", sender: nil)
+
     }
 }
